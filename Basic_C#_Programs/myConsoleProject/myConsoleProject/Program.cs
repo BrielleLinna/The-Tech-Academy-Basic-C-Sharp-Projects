94ec5cdda23a78f66945c8252131c6aab6d12a37
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-10-29 14:26:57 -0700

    Add files via upload

---
+++ Basic_C#_Programs/myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine(Brielle);
+        }
+    }
+}
