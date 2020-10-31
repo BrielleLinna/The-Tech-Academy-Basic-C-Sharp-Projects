1023b1a8afc7c2d2ecd494a1e983f61565d585ae
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-10-31 14:35:54 -0700

    Adding Math Operation Assignment

---
+++ Basic_C#_Programs/DailyReport/OperationAssignment/OperationAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OperationAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number:");
+            int x = Console.ReadLine() * 50;
+            Console.WriteLine("The value is:" + x);
+        }
+    }
+}
