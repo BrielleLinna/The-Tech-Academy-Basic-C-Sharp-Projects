698730281a6ad2c5d2aed95aec2cd5f897083a25
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-11-02 08:52:28 -0800

    Submitting Insurance Assignment

---
+++ Basic_C#_Programs/InsuranceAssignment/InsuranceAssignment/InsuranceAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InsuranceAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int ageInput = Convert.ToInt32(Console.ReadLine());
+          
+            Console.WriteLine("Have you ever had a DUI? True or False:");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            bool trueOrFalse = dui != true;
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTicket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualifed?");
+            bool qualified = (ageInput >=15 && trueOrFalse && speedingTicket <=3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();           
+
+        }
+    }
+}
