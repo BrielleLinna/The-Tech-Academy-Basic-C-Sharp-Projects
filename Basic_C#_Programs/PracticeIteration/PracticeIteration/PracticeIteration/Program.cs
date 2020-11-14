fc3c2241d990b6d85460a9a073462683ab21e7bf
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-11-13 16:55:30 -0800

    Updating Project PracticeIteration

--- Basic_C#_Programs/PracticeIteration/PracticeIteration/PracticeIteration/Program.cs
+++ Basic_C#_Programs/PracticeIteration/PracticeIteration/PracticeIteration/Program.cs
@@ -95,33 +95,26 @@ namespace PracticeIteration
             Console.WriteLine("Please write a type of animal:");
             string animal = Console.ReadLine();
 
-            if(identical.Contains(animal))
+            if (identical.Contains(animal))
             {
-                    Console.WriteLine("You chose" + animal);
-                    Console.ReadLine();
+                Console.WriteLine("You chose" + animal);
+                Console.ReadLine();
 
                 for (int i = 0; i < identical.Count(); i++)
                 {
-                        if (animal == [i])
-                        Console.WriteLine()
+                    if (animal == identical[i])
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.ReadLine();
                 }
+            }
             else
             {
                 Console.WriteLine("User input is not in the string.");
                 Console.ReadLine();
             }
 
-            for(int i = 0; i < identical.Count(); i++)
-            {
-                Console.WriteLine(identical.Contains(animal));
-
-            if (animal = identical[i])
-            {
-                    Console.WriteLine(IndexOf(animal));
-                    Console.ReadLine();
-            }
-
-            }
 
             //Assignment Part 6
 
@@ -130,44 +123,21 @@ namespace PracticeIteration
             six.Add("Goodmorning");
             six.Add("Goodnight");
             six.Add("Hello");
-            string saying = "Hello";
-
-            Console.WriteLine(six);
-
-            foreach (string w in six)
-            {
-                Console.WriteLine(w);
-                Console.ReadLine();
-            }
 
             List<string> newSix = new List<string>();
 
-            if (six.Contains(saying))
+            foreach (var saying in six)
             {
-                newSix.Add(saying);
-                Console.WriteLine(newSix);
-                Console.ReadLine();
-                //Not displaying a new list with just matching saying variable
+                if (!newSix.Contains(saying))
+                {
+                    newSix.Add(saying);
+                }
+                else
+                {
+                    Console.WriteLine("This is a duplicate." + saying);
+                }
             }
-        }
-
-            
-
-           
-
-        //Other attempts at how to add data from one list into an empty list using online research
-        //    var myList = new List<string>[] { new List<string>(), new List<string>() };
-
-           
-        //        if private static object firstlist;
-
-        //(string i.Contains("Hello"))
-        //            myList.Last().Add(i.ToString());
-        //        else
-        //            myList.First().Add(i.ToString());
-        //            Console.WriteLine(myList);
-        //            Console.ReadLine();
-
-        
+            Console.ReadLine();
+        } 
     }
 }
