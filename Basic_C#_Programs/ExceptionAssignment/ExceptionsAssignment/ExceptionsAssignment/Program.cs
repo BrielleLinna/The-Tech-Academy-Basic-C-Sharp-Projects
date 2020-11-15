0c0dc847d5241e5a691f61b00970b0ac39120fbe
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-11-14 20:27:49 -0800

    Uploading Exception Handling Assignment

---
+++ Basic_C#_Programs/ExceptionAssignment/ExceptionsAssignment/ExceptionsAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(4);
+            numbers.Add(6);
+            numbers.Add(8);
+
+            try
+            {
+                Console.WriteLine("Pick a number:");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number:");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + "divided by" + numberTwo + "equals" + numberThree);
+                Console.ReadLine();
+            }
+
+            catch(exception );
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/ExceptionAssignment/NewExeception/NewExeception/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            numbers.Add(20);
+            numbers.Add(40);
+            numbers.Add(60);
+            numbers.Add(80);
+
+            try
+            {
+
+                Console.WriteLine("Pick a number:");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int v in numbers)
+                {
+                    int numberThree = v / numberOne;
+                    Console.WriteLine(numberThree);
+                    Console.ReadLine();
+                }
+            }
+
+
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("please write a whole number, not text");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("you cannot divide by zero.");
+
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("You are now moving on in the program from this try/catch code.");
+            Console.ReadLine();
+            //Errors we are trying to exception handle
+            // DivideByZeroException
+            // system.formatexception
+
+            }
+        }
+    }
