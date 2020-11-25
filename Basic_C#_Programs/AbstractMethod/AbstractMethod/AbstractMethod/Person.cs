7e544db464728d9a3b66a8c10c7b286901da5b91
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-11-25 13:29:57 -0800

    Uploading Abstract Method

---
+++ Basic_C#_Programs/AbstractMethod/AbstractMethod/AbstractMethod/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractMethod
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void sayName()
+        {
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);
+        }
+       
+    }
+}
