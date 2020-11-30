163ebe23403a4fe3037619c063a18f2df82fa8f3
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-11-30 08:41:41 -0800

    Uploading Struct Assignment

---
+++ Basic_C#_Programs/StructAssignment/Struct/Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StructNumber newNumber = new StructNumber();
+
+            Console.WriteLine(newNumber.Amount = 2.1m);
+            Console.ReadLine();
+
+        }
+    }
+}
