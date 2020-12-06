5a1e06bef0540ac31369b8a7fb154e84999688d0
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-12-06 12:18:01 -0800

    Uploading DateTime Assignment

--- Basic_C#_Programs/DateTimeAssignment/DateTime/DateTimeAssignment/DateTimeAssignment/Program.cs
+++ Basic_C#_Programs/DateTimeAssignment/DateTime/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace DateTimeAssignment
 {
     class Program
     {
 
-
         public static void Main()
         {
             DateTime now = DateTime.Now;
             Console.WriteLine(now);
-       
-
+            
             Console.WriteLine("Please pick a number.");
             int answer = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("It will be", now + answer);
-            Console.ReadLine();
+            DateTime XhoursLater = now.AddHours(answer);
+       
+                Console.WriteLine("It will be:", XhoursLater);
+                Console.ReadLine();
 
+            }
         }
-    }
 }
+
+    
