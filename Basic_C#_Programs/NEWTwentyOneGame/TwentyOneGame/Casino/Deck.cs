bb5ea1f1e5aa89486c62000076ab50fc33a8a022
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-12-15 18:00:40 -0800

    Added Car Insurance Project

---
+++ Basic_C#_Programs/NEWTwentyOneGame/TwentyOneGame/Casino/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Faces = new List<string>()
+            {
+                "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King","Ace"
+            };
+
+            for(int i = 0; i < 13; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    card.Suit = (Suit)j;
+                    card.Face = (Face)i;
+                    Cards.Add(card);
+
+                }
+            }
+        }
+        public List<Card> Cards { get; set; }
+        public void Shuffle (int times = 1)
+        {
+            for (int i = 0; i< times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count >  0)
+                {
+                    int randomIndex = random.Next(Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = TempList;
+            }
+        }
+    }
+} 
--- Basic_C#_Programs/NEWTwentyOneGame/TwentyOneGame/TwentyOneGame/Program.cs
+++ Basic_C#_Programs/NEWTwentyOneGame/TwentyOneGame/TwentyOneGame/Program.cs
@@ -3,29 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwentyOne;
+using System.IO;
+using System.Data.SqlClient;
+using Casino;
+using Casino.TwentyOne;
 
-namespace TwentyOneGame
+namespace TwentyOne
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            const string casinoName = "Grand Hotel and Casino";
+
+            Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + "|");
+                    Console.Write(exception.ExceptionType + "|");
+                    Console.Write(exception.ExceptionMessage + "|");
+                    Console.Write(exception.TimeStamp + "|");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only.");
+            }
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y " || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\Brielle\Documents\Logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
+
                 Game game = new TwentyOneGame();
                 game += player;
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
-                {
-                    game.Play();
-                }
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
 
@@ -34,8 +85,68 @@ namespace TwentyOneGame
             Console.ReadLine();
 
             Deck deck = new Deck();
+            deck.Shuffle(3);
             
           
         }
+
+        //private static void UpdateDbWithException()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            string queryString =@"INSERT INTO dbo (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                (@ExceptiType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", System.Data.SqlDbType.VarChar);
+                command.Parameters.Add(@"ExceptionMessage", System.Data.SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", System.Data.SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            string queryString = @"Select.Id, ExceptionType, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
+        }
+
     }
 }
